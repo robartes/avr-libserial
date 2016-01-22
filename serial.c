0c2e014ae97b5152a9770424ae683795d49731c8
Bart Vetters <robartes@nirya.be>
2016-01-22 15:43:12 +0100

    WIP. Starting RX implementation

--- serial.c
+++ serial.c
@@ -8,13 +8,25 @@
 #include <stdint.h>
 #include "serial.h"
 
-#define NUM_SPEED  5 
+#define NUM_SPEED		   5 
+#define PRESCALER_DIVISOR   16
 
 /************************************************************************
- * state variables (global)
+ * File global variables
  ************************************************************************/
 
-serial_state_t connection_state = SERIAL_NOT_INITIALISED;
+static uint8_t connection_state = SERIAL_NOT_INITIALISED;
+
+struct {
+	uint8_t lock;
+	uint8_t *data;
+	uint8_t top;
+} buffer;
+
+static buffer rx_buffer = {0, NULL, 0};
+static buffer tx_buffer = {0, NULL, 0};
+
+static struct serial_config my_config;
 
 /************************************************************************
  * Private functions
@@ -70,8 +82,95 @@ static return_code_t setup_io(
 
 	}
 
-    return SERIAL_OK;
+	return SERIAL_OK;
 
+}
+
+static void move_connection_state(uint8_t src_state, uint8_t dst_state)
+{
+
+	connection_state &= ~src_state;
+	connection_state |= dst_state;
+
+}
+
+/************************************************************************
+ * Timer1 Compare Match A interrupt - main polling / transmit routine
+ ************************************************************************/
+
+volatile uint8_t rx_bit_counter = 0;
+volatile uint8_t tx_bit_counter = 0;
+volatile uint8_t rx_byte = 0;
+
+ISR(TIM1_COMPA_vect)
+{
+
+	// RX
+	if (get_connection_state(SERIAL_RECEIVED_START_BIT)) {
+
+		// First data bit
+
+		if (bit_is_set(*rx_port, rx_pin))
+			rx_byte |= (1 << rx_bit_counter);	
+		rx_bit_counter++;
+		move_connection_state(
+			SERIAL_RECEIVED_START_BIT,
+			SERIAL_RECEIVING_DATA 
+		);
+			
+
+	} else if (get_connection_state(SERIAL_RECEIVING_DATA)) {
+	
+		// Subsequent data bits or stop bit
+		switch (rx_byte_counter) {
+
+			case 8:
+
+				// Check for stop bit
+				if (bit_is_set(*rx_port, rx_pin)) {
+					rx_bit_counter = 0;
+					if (!rx_buffer.lock) {
+						store_data(*rx_buffer, rx_byte);
+						rx_byte = 0;
+						move_connection_state(
+							SERIAL_RECEIVING_DATA,
+							SERIAL_IDLE,
+						);
+					} else {
+						// Buffer was locked. Try again
+						move_connection_state(
+							SERIAL_RECEIVING_DATA,
+							SERIAL_RECEIVE_BUFFER_LOCKED,
+						);
+					}
+				} else {
+					// Weird - no stop bit after 8 data bits. Reset connection
+					move_connection_state(
+						SERIAL_RECEIVING_DATA,
+						SERIAL_IDLE,
+					);
+
+				}
+				break;
+
+			default:
+
+				break;
+		}
+
+    } else if (get_connection_state(SERIAL_RECEIVED_STOP_BIT)) {
+
+	} else {
+
+		// Not receiving anything right now. Check for start bit
+		if (bit_is_clear(*rx_port, rx_pin))
+			set_connection_state(SERIAL_RECEIVED_START_BIT);
+
+	}
+
+
+	
+	// TX
 }
 
 /************************************************************************
@@ -82,18 +181,20 @@ static return_code_t setup_io(
  * serial_initialise: set up connection
  * 
  * Parameters:
- *          struct serial *serial   Serial config structure 
+ *		  struct serial *serial   Serial config structure 
  *
  * Returns:
- *      SERIAL_ERROR on error
- *      SERIAL_OK otherwise
+ *	  SERIAL_ERROR on error
+ *	  SERIAL_OK otherwise
  *
  * This function:
+ *  - Mallocs rx & tx buffers
  *  - Sets up the I/O ports
  *  - Sets up the frame receive interrupt
  *  - Sets up & starts the timer to provide the 'clock'
  *
  * Possible errors are:
+ * 	- Not enough memory for buffers
  *  - Timer already running, so UART connection already started
  *  - No PCINT interrupt possible on RX pin   
  ************************************************************************/
@@ -102,34 +203,61 @@ static return_code_t setup_io(
 extern return_code_t serial_initialise(struct serial_config *config)
 {
 
-    uint32_t serial_speeds[NUM_SPEED] = {9600, 19200, 38400, 57600, 115200};
+	uint32_t serial_speeds[NUM_SPEED] = {9600, 19200, 38400, 57600, 115200};
+	uint8_t *rxd;
+	uint8_t *txd;
+
 
 	// Sanity checks. Timer running?
 	if (TCCR1 & 0x0f)
 		return SERIAL_ERROR;
-	
-    // Setup I/O
-    if (setup_io(config->tx_port, config->tx_pin, SERIAL_DIR_TX) != SERIAL_OK) {
-        return SERIAL_ERROR;
-    };
 
-    if (setup_io(config->rx_port, config->rx_pin, SERIAL_DIR_RX) != SERIAL_OK) {
-        return SERIAL_ERROR;
-    };
+	// Allocate buffers
+	if ((rxd = malloc(RX_BUFFER_SIZE)) == NULL)
+		return SERIAL_ERROR;
 
-    // Setup interrupt
-	
-	
+	if ((tx = malloc(TX_BUFFER_SIZE)) == NULL)
+		return SERIAL_ERROR;
+
+	rx_buffer.data = rxd;
+	tx_buffer.data = txd;
+
+	// Setup I/O
+	if (setup_io(config->tx_port, config->tx_pin, SERIAL_DIR_TX) != SERIAL_OK) {
+		return SERIAL_ERROR;
+	};
+
+	if (setup_io(config->rx_port, config->rx_pin, SERIAL_DIR_RX) != SERIAL_OK) {
+		return SERIAL_ERROR;
+	};
+
+	my_config.rx_port = rx_port;
+	my_config.rx_pin = rx_pin;
+	my_config.tx_port = tx_port;
+	my_config.tx_pin = tx_pin;
+
+	// Setup interrupt: Compare Match A interrupt Timer1
+	TIMSK |= (1 << OCIE1A);	
 
 	// Setup timer
+	// CTC Mode (clear on reaching OCR1C)
+	TCCR1 |= (1 << CTC1); 
+	OCR1A = OCR1C = (uint8_t) (F_CPU / PRESCALAR_DIVISOR / serial_speeds[config->speed]) - 1;
 
 	// Start timer. /16 prescaler - datasheet p.89 table 12-5
 	TCCR1 &= ~(1 << CS13 | 1 << CS12 | 1 << CS11 | 1 << CS10);
 	TCCR1 |= (1 << CS12 | 1 << CS10);
 
-   
-    connection_state = SERIAL_IDLE;
+	connection_state = SERIAL_IDLE;
 
-    return SERIAL_OK;
+	return SERIAL_OK;
 
 }
+
+extern uint8_t get_connection_state(uint8_t expected_state)
+{
+
+	return connection_state & expected_state;
+
+}
+
--- serial.h
+++ serial.h
@@ -4,6 +4,22 @@
  * AVR software serial library
  ************************************************************************/
 
+#define SERIAL_IDLE						0b00000000
+#define SERIAL_SENT_START_BIT			0b00000001
+#define SERIAL_SENDING_DATA				0b00000010
+#define SERIAL_SENT_STOP_BIT			0b00000100
+#define SERIAL_RECEIVED_START_BIT		0b00001000
+#define SERIAL_RECEIVING_DATA			0b00010000
+#define SERIAL_RECEIVE_BUFFER_LOCKED	0b00100000
+
+#define SERIAL_RECEIVING			0b00111000
+#define SERIAL_SENDING				0b00000111
+
+#define SERIAL_NOT_INITIALISED		0b10000000
+
+#define RX_BUFFER_SIZE				64			// In bytes
+#define TX_BUFFER_SIZE				64			// In bytes
+
 typedef enum {
 	SERIAL_SPEED_9600,
 	SERIAL_SPEED_19200,
@@ -12,16 +28,6 @@ typedef enum {
 	SERIAL_SPEED_115200,
 } serial_speed_t;
 
-typedef enum {
-	SERIAL_NOT_INITIALISED,
-	SERIAL_IDLE,
-	SERIAL_SENT_START_BIT,
-	SERIAL_SENDING_DATA,
-	SERIAL_SENT_STOP_BIT,
-	SERIAL_RECEIVED_START_BIT,
-	SERIAL_RECEIVING_DATA,
-	SERIAL_RECEIVED_STOP_BIT,
-} serial_state_t;
 
 struct serial_config {
 	serial_speed_t speed;
@@ -33,24 +39,26 @@ struct serial_config {
 
 typedef enum {
 	SERIAL_ERROR,
- 	SERIAL_OK,	
+	SERIAL_OK,	
 } return_code_t ;
 
 /************************************************************************
  * serial_initialise: set up connection
  * 
  * Parameters:
- *          struct serial_config *config   Serial config structure 
+ *		  struct serial_config *config   Serial config structure 
  *
  * Returns:
- *      ERROR on error
- *      OK otherwise
+ *	  ERROR on error
+ *	  OK otherwise
  *
  * This function:
+ *  - Mallocs RX & TX buffers
  *  - Starts the timer to provide the 'clock'
  *  - Sets up the I/O ports
  *  - Sets up the frame receive interrupt
  * Possible errors are:
+ *  - Not enough memory for buffers
  *  - Timer already running, so UART connection already started
  *  - No PCINT interrupt possible on RX pin   
  ************************************************************************/
@@ -61,11 +69,11 @@ extern return_code_t serial_initialise(struct serial_config *config);
  * serial_put_char: send a single byte
  *
  * Parameters:
- * 		struct serial_config *		Configuration structure of this connection
+ *		struct serial_config *		Configuration structure of this connection
  *		uint8_t data		Byte to send
  *
  * Returns:
- * 		uint8_t length 			Length of data sent out (should be 1)
+ *		uint8_t length			Length of data sent out (should be 1)
  ************************************************************************/
 
 extern uint8_t serial_put_char(struct serial_config *config, uint8_t data);
@@ -83,7 +91,7 @@ extern uint16_t serial_send_data(struct serial_config *config, uint8_t *data, ui
 extern uint16_t serial_data_pending(struct serial_config *config);
 
 /************************************************************************
- * serial_get_char     
+ * serial_get_char	 
  ************************************************************************/
 
 extern uint8_t serial_get_char(struct serial_config *config);
